3883f8ac9a0e99fcdd20c11b94a37c3ca814beb4
qwyx <mc010116@gmail.com>
2025-02-21 13:54:53 +0100

    feat(app): finish 20

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 const menu = document.querySelector('#menu');
+const btnGamesRules = document.querySelector('#btnGamesRules');
+
+btnGamesRules.addEventListener('click', () => {
+    const rulesOverlay = document.createElement('div');
+    rulesOverlay.innerHTML = `
+        <section class="secRules">
+            <div class="divRls">
+                <h2 class="h2R heading-l">RULES</h2>
+                <div class="divObj">
+                    <h3 class="heading-s">OBJECTIVE</h3>
+                    <p>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).</p>
+                </div>
+                <div class="divHplay">
+                    <h3 class="heading-s">HOW TO PLAY</h3>
+                    <ol>
+                        <li>Red goes first in the first game.</li>
+                        <li>Players must alternate turns, and only one disc can be dropped in each turn. </li>
+                        <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
+                        <li>The starter of the previous game goes second on the next game.</li>
+                    </ol>
+                </div>
+            </div>
+            <img src="asset/Group 2.png" alt="logoValider" class="logoValider">
+        </section>
+    `;
+    document.body.appendChild(rulesOverlay);
+
+    const retour = rulesOverlay.querySelector('.logoValider');
+    retour.addEventListener('click', () => {
+        document.body.removeChild(rulesOverlay);
+    });
+});
 
 menu.addEventListener('click', () => {
     const pauseOverlay = document.createElement('div');
